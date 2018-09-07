4cb390a4bee16829314c4e2ce28e3c80bae8d965
Sham Ramchandani <sham.ramchandani@ft.com>
2018-09-07 14:06:36 +0100

    more update

--- IsLeapYear.py
+++ IsLeapYear.py
@@ -1,6 +1,10 @@
 #This script ask the user for a Year and then decided is if it is a Leap Year or not 
-
-year = int(input("Please Enter the Year in numbers"))
+import sys
+try:
+    year = int(input("Please Enter the Year in numbers "))
+except ValueError:
+    print('You have to enter numbers only')
+    sys.exit()
 
 if year % 4 == 0:
     print('this is a leap year')
