900c2e44cc2d2ebfe728b28a6b414415524615e2
Sham Ramchandani <sham.ramchandani@ft.com>
2018-09-28 13:39:43 +0100

    created new folder for division by 3 and lib folder

---
+++ Divisionbythree/lib/division.py
@@ -0,0 +1,14 @@
+class DivisionProcessing:
+    
+    def __init__(self):
+        pass
+
+    def check_num(self, number):
+        while number > 9:
+            digit_list = list(str(number))
+
+            number = sum([int(d) for d in str(number)])
+        if number in (3,6,9):
+            return True
+        else:
+            return False
---
+++ Divisionbythree/lib/validation.py
@@ -0,0 +1,11 @@
+class ValidationProcessing:
+    
+    def __init__(self, args):
+        for k, v in args.items():
+            setattr(self, k, v)
+
+    def is_positive(self):
+        if self.number > 0:
+            return True
+        else:
+            return False
